ea632ba5c2e3ff77f65fb10c59aad0638faa693b
ranadheer314 <ranadheer314@users.noreply.github.com>
2022-07-16 12:48:00 -0400

    added pipeline,docker and ingest_data files

---
+++ ingest_data.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import re
+from numpy import require
+import pandas as pd
+import os
+from sqlalchemy import create_engine
+import pyarrow.parquet as pq
+from time import time
+import argparse
+
+def main(params):
+
+    user = params.user
+    password = params.password
+    host = params.host 
+    port = params.port 
+    db = params.db
+    table_name = params.table_name
+    url = params.url
+    filename = 'output.parquet'
+
+    os.system(f'wget {url} -O {filename}')
+    
+    engine=create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+
+
+    parquet_table=pq.read_table(filename)
+    df=parquet_table.to_pandas()
+
+    df.to_sql(name=f"{table_name}",con=engine,if_exists="append",chunksize=100000)
+
+    
+
+if __name__ == '__main__':
+
+    parser=argparse.ArgumentParser(description="something")
+
+
+    parser.add_argument('--user',required=True,help="username of the database")
+    parser.add_argument('--password',required=True,help="password for the database")
+    parser.add_argument('--host',required=True,help="hostname of the database")
+    parser.add_argument('--port', required=True, help='port for postgres')
+    parser.add_argument('--db', required=True, help='database name for postgres')
+    parser.add_argument('--table_name', required=True, help='name of the table where we will write the results to')
+    parser.add_argument('--url', required=True, help='url of the parquet file')
+
+    args = parser.parse_args()
+
+    main(args)
+    
+
